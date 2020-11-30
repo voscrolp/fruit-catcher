084d3e5132efc6b8699070c1d614d7ac504078e1
voscrolp <70542514+voscrolp@users.noreply.github.com>
2020-11-30 11:08:38 -0600

    Add files via upload

---
+++ js/player.js
@@ -0,0 +1,51 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+        this.score =0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    getPlayer1Score(){
+        database.ref('players/player1/score').on("value",(data) =>{
+            player1score = data.val();
+        });
+    }
+
+    getPlayer2Score(){
+        database.ref('players/player2/score').on("value",(data) =>{
+            player2score = data.val();
+        });
+    }
+
+    
+}
